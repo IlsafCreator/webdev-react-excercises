e5f3f4243df00a2ce82a8a97cd1b86103b683ef9
Ilsaf Khasanov <ilsaf.khasanov@mail.ru>
2020-05-04 17:21:59 +0300

    rewrite first exercise

--- excercise-1/webpack.config.js
+++ excercise-1/webpack.config.js
@@ -1,48 +1,24 @@
-const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  context: path.resolve(__dirname, 'src'),
-  entry: './main.js',
+  entry: "./src/index.js",
   output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.join(__dirname, "/dist"),
+    filename: "index-bundle.js"
   },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin(),
-      new OptimizeCssAssetsPlugin({
-      })
-    ],
-  },
-  plugins: [
-    new HTMLWebpackPlugin({
-      template: 'index.html',
-      minify: {
-        collapseWhitespace: true
-      }
-    }),
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css'
-    })
-  ],
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {},
-          },
-          'css-loader',
-        ]
-      }]
-  }
-}
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
+};
